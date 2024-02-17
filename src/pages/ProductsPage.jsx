3b9fdad8d742d049cb11c412a6cc87b78ef85a1e
HadiKia <hadikiamarzi@protonmail.com>
2024-02-17 17:16:35 +0330

    Render the product list

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -7,7 +7,7 @@ function ProductsPage() {
   const products = useProducts();
 
   return (
-    <div>
+    <>
       <div className="grid place-items-center">
         <img src={topSection} alt="section" className="w-full" />
         <img
@@ -16,7 +16,27 @@ function ProductsPage() {
           className="relative -top-3 md:-top-5 lg:-top-7 xl:-top-12 scale-90 xl:scale-[0.8] rounded-lg"
         />
       </div>
-    </div>
+
+      <div className="container mx-auto px-5 ">
+        <div className="flex flex-col md:flex-row items-start justify-between">
+          {/* sidebar */}
+          <div>sidebar Lorem, ipsum dolor sit amet consectetur</div>
+
+          {/* products */}
+          <div>
+            {!products.length && <p>Loading...</p>}
+            {products.map((product) => (
+              <div key={product.id}>
+                <img src={product.image} alt="" />
+                <span>{product.category}</span>
+                <p>{product.title}</p>
+                <p>${product.price}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
