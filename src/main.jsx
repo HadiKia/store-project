6a72d60e3f513e2fa995a5e1e9cd349ee4053679
HadiKia <hadikiamarzi@protonmail.com>
2024-03-06 17:00:27 +0330

    Creating store

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./app/store.js";
 import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <BrowserRouter>
+  <BrowserRouter>
+    <Provider store={store}>
       <App />
-    </BrowserRouter>
+    </Provider>
+  </BrowserRouter>
 );
