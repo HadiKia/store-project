afe59a1d8fde09e89a57645467e0f4edd4b3b3e5
HadiKia <hadikiamarzi@protonmail.com>
2024-03-07 16:20:44 +0330

    Creating cart slice

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -58,16 +58,13 @@ const getInitialQuery = (searchParams) => {
   return query;
 };
 
-const sumProducts = (products) => {
-  const itemsCounter = products.reduce(
-    (counter, product) => counter + product.quantity,
-    0
-  );
-  const total = products
+const sumPrice = (products) => {
+  return products
     .reduce((total, product) => total + product.price * product.quantity, 0)
     .toFixed(2);
-
-  return { itemsCounter, total };
+};
+const sumQuantity = (products) => {
+  return products.reduce((counter, product) => counter + product.quantity, 0);
 };
 
 const productQuantity = (state, id) => {
@@ -86,6 +83,7 @@ export {
   filterProducts,
   createQueryObject,
   getInitialQuery,
-  sumProducts,
+  sumPrice,
+  sumQuantity,
   productQuantity,
 };
